b8fe5123c6064ef8626114e6b242d717cecfd757
Stephen Rasku <mercurial@srasku.net>
2021-05-05 15:26:50 -0700

    Fix shellcheck issue.

--- in-container.sh
+++ in-container.sh
@@ -2,4 +2,4 @@
 
 IMAGE=srasku-dev:latest
 docker build -f Docker/Dockerfile -t ${IMAGE} .
-docker run -v $(pwd):/home/develop --rm ${IMAGE} "$@"
+docker run -v "$(pwd):/home/develop" --rm ${IMAGE} "$@"
