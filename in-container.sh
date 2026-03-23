45c5bd15c4b5369db70d3f0f9e782da45ad0be8a
Stephen Rasku <mercurial@srasku.net>
2026-03-23 13:23:09 -0700

    Create .claude.json if it doesn't exist
    Save it as `Docker/.claude.json` and mount it in the container.  This will
    save the configuration across sessions so you don't need to re-authorize every
    time.

--- in-container.sh
+++ in-container.sh
@@ -2,4 +2,11 @@
 
 IMAGE=claude-dev:latest
 DOCKER_BUILDKIT=0 docker build -f Docker/Dockerfile -t ${IMAGE} .
-docker run -it -v "$(pwd):/home/develop" --rm ${IMAGE} "$@"
+
+CLAUDE_SETTINGS=Docker/.claude.json
+test -s ${CLAUDE_SETTINGS} || echo "{}" > ${CLAUDE_SETTINGS}
+docker run \
+    -it \
+    -v "$(pwd):/home/develop" \
+    -v "$(pwd)/Docker/.claude.json:/root/.claude.json" \
+    --rm ${IMAGE} "$@"
